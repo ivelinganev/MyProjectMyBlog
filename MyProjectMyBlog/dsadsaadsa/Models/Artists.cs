86e87112f37890cdb5d2ef50aeea865f91ecdb70
Ivelin Ganev <nocontrol4y@yahoo.com>
2016-08-24 17:36:03 +0300

    Update
    bugs fixed

---
+++ MyProjectMyBlog/dsadsaadsa/Models/Artists.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyProjectMyBlog.Models
+{
+    public class Artists
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string From { get; set; }
+    }
+
+    // movie/random
+}
---
+++ MyProjectMyBlog/dsadsaadsa/ViewModels/RandomArtistViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyProjectMyBlog.Models;
+
+namespace MyProjectMyBlog.ViewModels
+{
+    public class RandomArtistViewModel
+    {
+        public Artists Artist { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
